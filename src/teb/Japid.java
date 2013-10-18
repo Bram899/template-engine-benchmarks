42572915460caea6aef57d41d8aa233f0347e8b5
unknown <name.is.carl@gmail.com>
2013-10-18 21:56:05 +0200

    Japid

--- src/teb/Japid.java
+++ src/teb/Japid.java
@@ -17,11 +17,14 @@ public class Japid extends _BenchBase {
 	private String template = "japid";
 
 	public Japid() {
+		cn.bran.japid.util.JapidFlags.verbose = false;
+		JapidRenderer.init(OpMode.dev, "templates", 1, null);
+
 	}
 
 	@Override
-	public void execute(Writer w0, Writer w1, int ntimes, List<Stock> items)
-			throws Exception {
+	public void execute(final Writer w0, final Writer w1, int ntimes,
+			final List<Stock> items) throws Exception {
 		while (--ntimes >= 0) {
 			if (ntimes == 0) {
 				String result = renderToString(items);
@@ -36,8 +39,8 @@ public class Japid extends _BenchBase {
 	}
 
 	@Override
-	public void execute(OutputStream o0, OutputStream o1, int ntimes,
-			List<Stock> items) throws Exception {
+	public void execute(final OutputStream o0, final OutputStream o1,
+			int ntimes, final List<Stock> items) throws Exception {
 		while (--ntimes >= 0) {
 			if (ntimes == 0) {
 				String result = renderToString(items);
@@ -52,7 +55,8 @@ public class Japid extends _BenchBase {
 	}
 
 	@Override
-	protected String execute(int ntimes, List<Stock> items) throws Exception {
+	protected String execute(int ntimes, final List<Stock> items)
+			throws Exception {
 		String result = null;
 		while (--ntimes >= 0) {
 			result = renderToString(items);
@@ -60,13 +64,13 @@ public class Japid extends _BenchBase {
 		return result;
 	}
 
-	private String renderToString(List<Stock> items) {
-		JapidRenderer.init(OpMode.dev, "templates", 1, null);
+	private String renderToString(final List<Stock> items) {
+
 		String output = JapidRenderer.renderWith(template, items);
 		return output;
 	}
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		new Japid().run();
 	}
 
